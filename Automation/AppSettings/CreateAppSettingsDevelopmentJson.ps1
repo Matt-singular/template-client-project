841f054d1e685a4ef80b28d353f87c400eea026b
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2025-09-19 21:03:07 +0200

    Added automation script to create the user's Application.Development.json

---
+++ Automation/AppSettings/CreateAppSettingsDevelopmentJson.ps1
@@ -0,0 +1,8 @@
+# Paths
+$scriptPath = $PSScriptRoot; # Root\Automation\AppSettings
+$applicationPath = Resolve-Path (Join-Path $scriptPath '..\..\Server\Application.API') # Root\Server\Application.API
+$templateFilePath = Resolve-Path (Join-Path $scriptPath 'Templates\AppsettingsDevelopmentTemplate.json') # Root\Automation\AppSettings\Templates\AppsettingsDevelopmentTemplate.json
+
+# Generate AppSettings.Development.json
+copy-item -path $templateFilePath -destination "$applicationPath\appsettings.Development.json" –confirm:$false
+Write-Host "Created appsettings.Development.json" -ForegroundColor Green
