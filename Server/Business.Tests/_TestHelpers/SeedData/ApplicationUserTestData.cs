400362ddbc6c7baa04c0aac023ce3cf4fbd16fb5
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2025-09-20 15:04:17 +0200

    Added ApplicationUserTestData

---
+++ Server/Business.Tests/_TestHelpers/SeedData/ApplicationUserTestData.cs
@@ -0,0 +1,38 @@
+namespace Business.Tests._TestHelpers.SeedData;
+
+using Business.Core.Models.Entities;
+using Business.Infrastructure.Contexts;
+
+/// <summary>
+/// Test data for the <see cref="ApplicationUser"/> entity.
+/// </summary>
+public static class ApplicationUserTestData
+{
+  /// <summary>
+  /// Gets a list of <see cref="ApplicationUser"/> instances for use in unit tests.
+  /// </summary>
+  /// <returns>The <see cref="ApplicationUser"/> entities used for testing.</returns>
+  public static List<ApplicationUser> GetTestUsers()
+  {
+    return
+    [
+      SeedData.GetApplicationSystemUser(),
+      new ApplicationUser
+      {
+        UserId = 2,
+        FirstName = "Alan",
+        Surname = "Turing",
+        UserName = "alanturing",
+        Email = "alanturing@testmail.com"
+      }.SetAuditFields(),
+      new ApplicationUser
+      {
+        UserId = 3,
+        FirstName = "Megan",
+        Surname = "Smith",
+        UserName = "megansmith",
+        Email = "megansmith@testmail.com"
+      }.SetAuditFields(),
+    ];
+  }
+}
