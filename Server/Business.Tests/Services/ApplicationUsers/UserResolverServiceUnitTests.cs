b505a53c02f41e4415b762d34907dcf9be547f23
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2025-10-22 07:21:29 +0200

    Added a UserResolverService unit test to verify the error handling functionality

--- Server/Business.Tests/Services/ApplicationUsers/UserResolverServiceUnitTests.cs
+++ Server/Business.Tests/Services/ApplicationUsers/UserResolverServiceUnitTests.cs
@@ -16,7 +16,7 @@ public class UserResolverServiceUnitTests
   /// </summary>
   /// <remarks>
   /// This test verifies that the system user is correctly retrieved from the database context
-  /// and that its properties match the expected values defined in <see cref="ApplicationConstants.SystemUserName"/>.
+  /// and that its properties match the expected values defined in <see cref="SeedData.GetApplicationSystemUser"/>.
   /// </remarks>
   [Fact]
   public async Task GetSystemUser_ShouldReturnSystemUser()
@@ -25,14 +25,32 @@ public class UserResolverServiceUnitTests
     using AppDbContext dbContext = await TestHelpers // TODO: will change how this is done.
       .CreateInMemoryDbContext()
       .AddTestSeedDataAsync();
-
     UserResolverService userResolverService = new(dbContext);
 
     // Act
-    ApplicationUser systemUser = Assert.IsType<ApplicationUser>(userResolverService?.GetSystemUser());
+    ApplicationUser systemUser = Assert.IsType<ApplicationUser>(userResolverService.GetSystemUser());
 
     // Assert
     Assert.Equal(1, systemUser.UserId);
     Assert.Equal(ApplicationConstants.SystemUserName, systemUser.UserName);
+  }
+
+  /// <summary>
+  /// Tests the error handling of <see cref="UserResolverService.GetSystemUser"/> when the system user is not configured.
+  /// </summary>
+  /// <remarks>This test verifies that there is sufficient error handling should the service not find a configured system user.</remarks>
+  [Fact]
+  public async Task GetSystemUser_WhenUserNotConfigured_ShouldThrowException()
+  {
+    // Arrange
+    string expectedError = string.Format(FriendlyErrorConstants.UserNotFound, ApplicationConstants.SystemUserName);
+    using AppDbContext dbContext = TestHelpers.CreateInMemoryDbContext(); // TODO: will change how this is done
+    UserResolverService userResolverService = new(dbContext);
+
+    // Act
+    InvalidOperationException actualError = Assert.Throws<InvalidOperationException>(userResolverService.GetSystemUser);
+
+    // Assert
+    Assert.Equal(expectedError, actualError.Message);
   }
 }
