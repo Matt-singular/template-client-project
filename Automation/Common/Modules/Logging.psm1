9875f3ed5145a4c59b9fe372c76494e242ad0a7a
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2025-11-04 16:49:17 +0200

    Updated logging PowerShell module functionality

--- Automation/Common/Modules/Logging.psm1
+++ Automation/Common/Modules/Logging.psm1
@@ -1,11 +1,38 @@
 function Write-Success {
-    param([string]$Message)
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Message
+    )
+
     if ($?) { Write-Host $Message -ForegroundColor Green }
 }
 
-function Write-Error {
-    param([string]$Message)
+function Write-Failure {
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Message
+    )
+
     if (-not $?) { Write-Host $Message -ForegroundColor Red }
 }
 
-Export-ModuleMember -Function Write-Success, Write-Error
+function Write-Result {
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$SuccessMessage,
+
+        [Parameter(Mandatory, Position=1)]
+        [string]$FailureMessage
+    )
+
+    if ($?) 
+    { 
+        Write-Host $SuccessMessage -ForegroundColor Green 
+    }
+    else
+    {
+        Write-Host $FailureMessage -ForegroundColor Red
+    }
+}
+
+Export-ModuleMember -Function Write-Success, Write-Failure, Write-Result
