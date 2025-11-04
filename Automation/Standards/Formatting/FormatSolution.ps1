6679306f5b440bf11f9ad4772db812ae61d1878e
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2025-11-04 16:49:57 +0200

    Standards, formatting, and tidy up

--- Automation/Standards/Formatting/FormatSolution.ps1
+++ Automation/Standards/Formatting/FormatSolution.ps1
@@ -4,13 +4,13 @@ Import-Module "$modulePath\Logging.psm1"
 Import-Module "$modulePath\Navigation.psm1"
 
 # Paths
-$server = Resolve-Path(Find-Up -FolderName 'Server')
+$server = Find-Up -FolderName 'Server'
+Set-Location $server
 
 # Formatting
-Set-Location $server
 dotnet format --include-generated --severity info --verbosity diagnostic
 #dotnet format --include-generated --exclude ..\Client\Application.GUI --severity info --verbosity diagnostic
 #dotnet format whitespace --verbosity diagnostic
 #dotnet format style --verbosity diagnostic
 #dotnet format analyzers --verbosity diagnostic --severity error
-Write-Success -Message 'Formatted solution'
+Write-Result 'Successfully formatted solution' 'Failed to format solution'
